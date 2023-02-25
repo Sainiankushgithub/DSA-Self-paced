38bd88b1c535aaba075ed65aa0cf284aa8e3d69a
sainiankushgithub <www.ankushsaini55@gmail.com>
2023-02-25 22:42:19 +0530

    programm

---
+++ DSA_Gfg/Arrays/ques17.cpp
@@ -0,0 +1,28 @@
+// MAXIMUM DIFFERNCE
+/*Maximum value of arr[j]-arr[i]*/
+#include<bits/stdc++.h>
+using namespace std;
+void display(int arr[],int size)
+{
+    for(int i=0;i<size;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+int main()
+{
+    int size;
+    cout<<"Enter the size of an array\n";
+    cin>>size;
+    int arr[size];
+    cout<<"Enter the numbers of an elements of an array\n";
+    for(int i=0;i<size;i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<"Displaying the array elemets before getting any leader elements\n";
+    display(arr,size);
+    cout<<"Displaying your leader elements\n";
+    return 0;
+}
