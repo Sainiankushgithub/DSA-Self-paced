491eb5227b4c166c9f28acda54395d3e4c6db3d1
sainiankushgithub <www.ankushsaini55@gmail.com>
2023-03-03 19:23:32 +0530

    programms

--- DSA_Gfg/Arrays/ques23.cpp
+++ DSA_Gfg/Arrays/ques23.cpp
@@ -1,13 +1,19 @@
-// 
+// PRINTIGN THE SUB ARRAY
 #include<bits/stdc++.h>
 using namespace std;
 void display(int arr[],int size)
 {
     for(int i=0;i<size;i++)
     {
-        cout<<arr[i]<<" ";
+        for(int j=0;j<size;j++)
+        {
+            for(int k=i;k<=j;k++)
+            {
+                cout<<arr[k]<<" ";
+            }
+            cout<<endl;
+        }
     }
-    cout<<endl;
 }
 int main()
 {
@@ -20,6 +26,7 @@ int main()
     {
         cin>>arr[i];
     }
+    cout<<"Displaying your sub array elements\n";
     display(arr,size);
 return 0;
 }
