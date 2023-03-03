3730318e222d6bc206e3886e8bd4edf84b0af9d8
sainiankushgithub <www.ankushsaini55@gmail.com>
2023-03-03 20:02:42 +0530

    programms

--- DSA_Gfg/Arrays/ques23.cpp
+++ DSA_Gfg/Arrays/ques23.cpp
@@ -1,13 +1,22 @@
-// MAXIMUM SUM SUBARRAY
+// MAXIMUM SUB ARRAY SUM 
 #include<bits/stdc++.h>
 using namespace std;
 void display(int arr[],int size)
 {
+    int res=arr[0];
     for(int i=0;i<size;i++)
     {
-        cout<<arr[i]<<" ";
+        for(int j=0;j<size;j++)
+        {
+            int sum=0;
+            for(int k=i;k<=j;k++)
+            {
+               sum+=arr[k];
+            }
+            res=max(res,sum);
+        }
     }
-    cout<<endl;
+    cout<<"Maximum sum is "<<res;
 }
 int main()
 {
@@ -20,6 +29,7 @@ int main()
     {
         cin>>arr[i];
     }
+    cout<<"Displaying your sub array elements\n";
     display(arr,size);
 return 0;
 }
