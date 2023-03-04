5a063bbeb8c1f8e0c0e71a706ddd9b3df4d35c98
sainiankushgithub <www.ankushsaini55@gmail.com>
2023-03-04 23:33:26 +0530

    programms

--- DSA_Gfg/Arrays/ques25.cpp
+++ DSA_Gfg/Arrays/ques25.cpp
@@ -1,6 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main()
+void display(int arr[],int size)
 {
+    for(int i=0;i<size;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+int main()
+{                                                          
+   int n;
+   cout<<"Enter the size of an  array\n";
+   cin>>n;
+   int arr[n];
+   cout<<"Enter the numbers of an elements of an array\n";               // METHOD 3
+   for(int i=0;i>n;i++)
+   {
+    cin>>arr[i];
+   }
+   display(arr,n);
 return 0;
 }
