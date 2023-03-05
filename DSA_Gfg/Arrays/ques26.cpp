025a11608ab68749ea9b3143ead135ce5917a6f5
sainiankushgithub <www.ankushsaini55@gmail.com>
2023-03-05 19:38:50 +0530

    programms

--- DSA_Gfg/Arrays/ques26.cpp
+++ DSA_Gfg/Arrays/ques26.cpp
@@ -9,17 +9,40 @@ void display(int arr[],int size)
     }
     cout<<endl;
 }
+int MaxlenOddEven(int arr[],int n)
+{
+    int res=1;
+    for(int i=0;i<n;i++)
+    {
+        int count=1;
+        for(int j=i+1;j<n;j++)                                        // METHOD 1
+        {
+            if((arr[j]%2==0)&&(arr[j-1]%2!=0)||(arr[j]%2!=0)&&(arr[j-1]%2==0))
+            {
+                count++;
+            }
+            else
+            {
+                break;
+            }
+        }
+        res=max(res,count);
+    }
+    return res;
+}
 int main()
 {                                                          
    int n;
    cout<<"Enter the size of an  array\n";
    cin>>n;
    int arr[n];
-   cout<<"Enter the numbers of an elements of an array\n";             // METHOD 1           
-   for(int i=0;i>n;i++)
+   cout<<"Enter the numbers of an elements of an array\n";                     
+   for(int i=0;i<n;i++)
    {
     cin>>arr[i];
    }
+   cout<<"Displaying your array\n";
    display(arr,n);
+   cout<<"Maximum length even-odd  is : "<<MaxlenOddEven(arr,n);
 return 0;
 }
