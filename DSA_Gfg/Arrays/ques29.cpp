30502cd5ca85c971a6346514ae54fc8befe20a83
sainiankushgithub <www.ankushsaini55@gmail.com>
2023-03-07 21:53:27 +0530

    programms

--- DSA_Gfg/Arrays/ques29.cpp
+++ DSA_Gfg/Arrays/ques29.cpp
@@ -1,1 +1,27 @@
 // MAJORITY ELEMENT IN AN ARRAY
+#include<bits/stdc++.h>
+using namespace std;
+void display(int arr[],int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+int main()
+{
+    int n;
+    cout<<"Enter the size of an array\n";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter the numbers of an elements of an array\n";
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<"Displaying your arrays elements\n";
+    display(arr,n);
+    cout<<"Maximum circular sub array sum is : "<<maxCirSubarray(arr,n);
+return 0;
+}
