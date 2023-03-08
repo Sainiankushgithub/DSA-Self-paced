3847110f251cd0b5f0c2d5eb8370d2836d8f43f5
sainiankushgithub <www.ankushsaini55@gmail.com>
2023-03-08 23:45:55 +0530

    programms

---
+++ DSA_Gfg/Arrays/ques30.cpp
@@ -0,0 +1,27 @@
+// MAJORITY ELEMENT IN AN ARRAY
+// IF COUNT GREATER THAN N/2 PRINT ANY INDEX OF THE ELEMENT ,ELSE RETURN -1
+#include<bits/stdc++.h>
+using namespace std;
+void display(int arr[],int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+int main()
+{
+    int n;
+    cout<<"Enter the size of an array\n";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter the numbers of an elements of an array\n";
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<"Displaying your arrays elements\n";
+    display(arr,n);
+return 0;
+}
