4b94cdb77c34aa566ac34d25253a7f5c0c189244
sainiankushgithub <www.ankushsaini55@gmail.com>
2023-03-11 20:41:05 +0530

    programm

--- DSA_Gfg/Arrays/ques32.cpp
+++ DSA_Gfg/Arrays/ques32.cpp
@@ -9,6 +9,27 @@ void display(bool arr[],int n)
     }
     cout<<endl;
 }
+void minFlip(bool arr[],int n)
+{
+    for(int i=1;i<n;i++)
+    {
+        if(arr[i]!=arr[i-1])
+        {
+            if(arr[i]!=arr[0])
+            {
+                cout<<"From "<<i<<" to ";
+            }
+            else
+            {
+                cout<<i-1<<endl;
+            }
+        }
+    }
+    if(arr[n-1]!=arr[0])
+    {
+        cout<<n-1;
+    }
+}
 int main()
 {
     int n;
@@ -22,5 +43,6 @@ int main()
     }
     cout<<"Displaying your arrays elements\n";
     display(arr,n);
+    minFlip(arr,n);
     return 0;
 }
