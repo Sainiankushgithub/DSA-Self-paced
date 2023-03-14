32953f96819c1df3e0d3e5843a77a379ca8ecf1d
sainiankushgithub <www.ankushsaini55@gmail.com>
2023-03-14 23:10:44 +0530

    programs

--- DSA_Gfg/Arrays/ques36.cpp
+++ DSA_Gfg/Arrays/ques36.cpp
@@ -9,17 +9,40 @@ void display(int arr[],int n)
     }
     cout<<endl;
 }
+bool givensum(int arr[],int n, int sum1)
+{
+    int res=arr[0];
+    for(int i=0;i<n;i++)
+    {
+        int sum=0;
+        for(int j=i;j<n;j++)
+        {
+            sum=sum+arr[j];
+            if(sum==sum1)
+            {
+                return true;
+                break;
+            }
+        }
+    }
+    return false;
+}
 int main()
 {
-     int n;
-    cout<<"Enter the size of an array\n";
+     int n;                                                                 // MY SOLUTION 
+    cout<<"Enter the size of an array\n";                                   // METHOD 1 : MY NAVIE APPROACH TO FIND OUT 
     cin>>n;
     int arr[n];
     cout<<"Enter the numbers of an elements of an array\n";
     for(int i=0;i<n;i++)
     {
-        cin>>arr[i];
+        cin>>arr[i];                      // TAKING INPUT OF ALL POSITIVE INTEGER 
     }
+    cout<<"Displaying your arrays elements are  \n";
     display(arr,n);
+    cout<<"Enter the given sum \n";
+    int sum;
+    cin>>sum;
+    cout<<"Given sum is presented in array are or not : "<<givensum(arr,n,sum);
 return 0;
 }
