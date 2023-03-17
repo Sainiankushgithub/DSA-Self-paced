48be1cd1edf9d708e7fb31a9d9eeff52c5de422f
sainiankushgithub <www.ankushsaini55@gmail.com>
2023-03-17 23:31:57 +0530

    programms

---
+++ DSA_Gfg/Arrays/ques41.cpp
@@ -0,0 +1,26 @@
+// EQULIBRIUM POINT 
+#include<bits/stdc++.h>
+using namespace std;
+void display(int arr[],int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+int main()
+{
+     int n;
+    cout<<"Enter the size of an array\n";                                         // METHOD 2 : 
+    cin>>n;
+    int arr[n];
+    cout<<"Enter the numbers of an elements of an array\n";                       // EFFICIENT SOLUTION 
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<"Displaying your array\n";
+    display(arr,n);
+return 0;
+}
