cc8a3cce17260c199d4c58178ae6b6319b944bbb
sainiankushgithub <www.ankushsaini55@gmail.com>
2023-03-18 23:55:05 +0530

    programm

--- DSA_Gfg/Arrays/ques41.cpp
+++ DSA_Gfg/Arrays/ques41.cpp
@@ -9,18 +9,38 @@ void display(int arr[],int n)
     }
     cout<<endl;
 }
+bool EquiPoint(int arr[],int n)
+{
+    int rs=0;
+    for(int i=0;i<n;i++)
+    {
+        rs+=arr[i];
+    }
+    int ls=0;
+    for(int i=0;i<n;i++)
+    {
+        rs-=arr[i];
+        if(ls==rs)
+        {
+            return true;
+        }
+        ls+=arr[i];
+    }
+    return false;
+}
 int main()
 {
      int n;
     cout<<"Enter the size of an array\n";                                         // METHOD 2 : 
     cin>>n;
     int arr[n];
-    cout<<"Enter the numbers of an elements of an array\n";                       // EFFICIENT SOLUTION 
+    cout<<"Enter the numbers of an elements of an array\n";                       // EFFICIENT SOLUTION : TIME : THETA(n)     AUX SPACE : O(1)
     for(int i=0;i<n;i++)
     {
         cin>>arr[i];
     }
     cout<<"Displaying your array\n";
     display(arr,n);
+    cout<<"Equilibrium point exist in the array or not : "<<EquiPoint(arr,n);
 return 0;
 }
