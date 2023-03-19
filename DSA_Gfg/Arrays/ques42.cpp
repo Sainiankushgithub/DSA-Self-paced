b2b30290e2696d5bf7c31e549f9e639307e4e7bc
sainiankushgithub <www.ankushsaini55@gmail.com>
2023-03-19 23:13:15 +0530

    progrsmms

--- DSA_Gfg/Arrays/ques42.cpp
+++ DSA_Gfg/Arrays/ques42.cpp
@@ -1,26 +1,43 @@
 // MAXIMUM APPEARING ELEMENT IN AN ARRAY 
 #include<bits/stdc++.h>
 using namespace std;
-void display(int arr[],int n)
+void display(int arrleft[], int arrright[],int n)
 {
+    cout<<"Displaying the left array elements \n";
     for(int i=0;i<n;i++)
     {
-        cout<<arr[i]<<" ";
+        cout<<arrleft[i]<<" ";
     }
     cout<<endl;
+    cout<<"Displaying the right array elements \n";
+    for(int i=0;i<n;i++)
+    {
+        cout<<arrright[i]<<" ";
+    }
+    cout<<endl;
+}
+int maxappear(int arrleft[],int arrright[],int n)
+{
+
 }
 int main()
 {
      int n;
     cout<<"Enter the size of an array\n";                                         // METHOD 1 : 
     cin>>n;
-    int arr[n];
-    cout<<"Enter the numbers of an elements of an array\n";                       // EFFICIENT SOLUTION 
+    int arrleft[n];
+    int arrright[n];
+    cout<<"Enter the numbers of an elements of the left array\n";                       // EFFICIENT SOLUTION 
     for(int i=0;i<n;i++)
     {
-        cin>>arr[i];
+        cin>>arrleft[i];
+    }
+    cout<<"Enter the numbers of an elements of the right array\n";                       // EFFICIENT SOLUTION 
+    for(int i=0;i<n;i++)
+    {
+        cin>>arrright[i];
     }
     cout<<"Displaying your array\n";
-    display(arr,n);
+    display(arrleft,arrright,n);
 return 0;
 }
