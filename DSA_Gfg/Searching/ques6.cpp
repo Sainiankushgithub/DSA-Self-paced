72ec4d0f6b2b1ae7b87303ffc1f180fd6c92ecc5
sainiankushgithub <www.ankushsaini55@gmail.com>
2023-03-21 15:34:54 +0530

    programms

---
+++ DSA_Gfg/Searching/ques6.cpp
@@ -0,0 +1,58 @@
+// INDEX OF FIRST OCCURENCE USING ITERATIVE METHOD
+#include<bits/stdc++.h>
+using namespace std;
+void display(int arr[],int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }                                                                          // METHOD 3 :  EFFICIENT SOLUTION
+    cout<<endl;
+}
+int Binarysearch(int arr[],int low,int high,int ele)
+{         
+    int mid;                   
+    while(low<=high)
+    {
+        cout<<"Ankush saini "<<endl;
+        mid=(low+high)/2;
+        if(arr[mid]==ele)
+        {
+            if(arr[mid-1]!=arr[mid]||mid==0)
+            {
+                return mid;
+            }
+            else
+            {
+                high=mid-1;
+            }
+        }
+        else if(arr[mid]>ele)
+        {
+            high=mid-1;
+        }
+        else{
+            low=mid+1;
+        }
+    }
+    return -1;
+}
+int main()
+{
+    int n;
+    cout<<"Enter the size of an array\n";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter the numbers of an elments in a sorted manner\n";
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<"Displaying your array\n";
+    display(arr,n);
+    int ele;
+    cout<<"Enter the element to be search\n";
+    cin>>ele;
+    cout<<"\nElement present at the index : "<<Binarysearch(arr,0,n-1,ele);
+    return 0;
+}
