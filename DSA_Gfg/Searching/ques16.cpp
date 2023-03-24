a8093c5e139d9f2c07ea264c59922fa79a788aad
sainiankushgithub <www.ankushsaini55@gmail.com>
2023-03-24 23:26:06 +0530

    programms

---
+++ DSA_Gfg/Searching/ques16.cpp
@@ -0,0 +1,17 @@
+// SEARCH IN A ROTATED SORTED ARRAY
+#include<bits/stdc++.h>
+using namespace std;
+int rotatedsorted(int arr[],int n,int ele)                            // EFFICIENT SOLUTION : 
+{
+
+}
+int main()
+{
+    int arr[]={10,20,30,40,8,9}; // HERE WE HAVE TAKEN A ROTAED SORTED ARRAY
+    int n=sizeof(arr)/sizeof(int);
+    int ele;
+    cout<<"Enter the element to be search\n";
+    cin>>n;
+    rotatedsorted(arr,n,ele);
+return 0;
+}
