8effce9f052e388ed125ac79836f1d1300a75247
sainiankushgithub <www.ankushsaini55@gmail.com>
2023-03-31 23:14:27 +0530

    programm

---
+++ DSA_Gfg/Searching/ques28.cpp
@@ -0,0 +1,12 @@
+// REPEAT ELEMENT EFFICIENT SOLUTION
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enterthe size of an array\n";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter the numbres ";
+return 0;
+}
