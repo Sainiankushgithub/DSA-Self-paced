cfd0ed4fdc62f059f18c09a5c8c606fa6fde89ad
sainiankushgithub <www.ankushsaini55@gmail.com>
2023-04-19 16:08:34 +0530

    programm

--- DSA_Gfg/Sorting/practice2.cpp
+++ DSA_Gfg/Sorting/practice2.cpp
@@ -88,6 +88,40 @@ void insertionSort(int arr[],int n)
         arr[j+1]=key;
     }
 }
+// MEERGE SORT 
+void mergeSort(int arr[],int low,int mid,int high)
+{
+    int n1=mid-low+1,n2=high-mid;
+    int left[n1],right[n2];
+    for(int i=0;i<n1;i++)
+    {
+        left[i]=arr[low+i];
+    }
+    for(int i=0;i<n2;i++)
+    {
+        right[i]=arr[mid+i+1];
+    }
+    int i=0,j=0,k=low;
+    while(i<n1&&j<n2)
+    {
+        if(left[i]<=right[i])
+        {
+            arr[k++]=left[i++];
+        }
+        else
+        {
+            arr[k++]=right[j++];
+        }
+    }
+    while(i<n1)
+    {
+        arr[k++]=left[i++];
+    }
+    while(j<n2)
+    {
+        arr[k++]=right[j++];
+    }
+}
 int main()
 {
     int n;
@@ -110,8 +144,11 @@ int main()
     /*cout<<"Displaying your array through selection sort\n";
     selectionSortII(arr,n);
     display(arr,n);*/
-    cout<<"Displaying your array through insertion sort\n";
-    insertionSort(arr,n);
+    // cout<<"Displaying your array through insertion sort\n";
+    // insertionSort(arr,n);
+    // display(arr,n);
+    cout<<"Displaying your array through merge sort\n";
+    mergeSort(arr,0,0+n/2,n-1);
     display(arr,n);
     return 0;
 }
