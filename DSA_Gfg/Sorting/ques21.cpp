7f268a028adccc5c090e54c9980cc1d4dc9893de
sainiankushgithub <www.ankushsaini55@gmail.com>
2023-04-25 23:16:57 +0530

    programm

--- DSA_Gfg/Sorting/ques21.cpp
+++ DSA_Gfg/Sorting/ques21.cpp
@@ -1,7 +1,26 @@
 // QUICK SORT
 #include<bits/stdc++.h>
 using namespace std;
+void display(int arr[],int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
 int main()
 {
+    int n;
+    cout<<"Enter the size of an array\n";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter the numbers of an elements of an array\n";
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<"Displaying your entered array elements \n";
+    display(arr,n);
 return 0;
 }
