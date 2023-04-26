e5b6c7e8ca11f6764c7ee800f6ff8bc130b0c497
sainiankushgithub <www.ankushsaini55@gmail.com>
2023-04-26 20:43:22 +0530

    programm

--- DSA_Gfg/Sorting/ques20.cpp
+++ DSA_Gfg/Sorting/ques20.cpp
@@ -9,6 +9,30 @@ void display(int arr[],int n)
     }
     cout<<endl;
 }
+int hoarePartition(int arr[],int l,int h)
+{
+    int i=l-1,j=h+1;
+    int pivot=arr[l];
+    while(true)
+    {
+        do
+        {
+            i++;
+        }while(arr[i]<pivot);
+        do
+        {
+            j--;
+        } while (arr[j]>pivot);                              // HOARE'S PARTITION IS BETTER ONLY IN LESS COMPARSION AS COMPARED TO LOUMTO PARTITION
+        if(i>=j)                                             // NOT STABLE ALGORITHM
+        {
+            return j;
+        }
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+
+}
 int main()
 {
     int n;
@@ -21,6 +45,9 @@ int main()
         cin>>arr[i];
     }
     cout<<"Displaying your entered array elements \n";
+    display(arr,n);
+    cout<<"Idex getting using hoare's partition : "<<hoarePartition(arr,0,n-1)<<endl;
+    cout<<"Array changes using hoare's partition \n";
     display(arr,n);
 return 0;
 }
