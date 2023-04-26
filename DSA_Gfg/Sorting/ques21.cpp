a66f19ecb6c22d9beb3c810e708a7230c62863e8
sainiankushgithub <www.ankushsaini55@gmail.com>
2023-04-26 22:33:22 +0530

    programm

--- DSA_Gfg/Sorting/ques21.cpp
+++ DSA_Gfg/Sorting/ques21.cpp
@@ -1,4 +1,4 @@
-// QUICK SORT
+// QUICK SORT ALGORITHM USING LOMUTO PARTITION 
 #include<bits/stdc++.h>
 using namespace std;
 void display(int arr[],int n)
@@ -8,6 +8,34 @@ void display(int arr[],int n)
         cout<<arr[i]<<" ";
     }
     cout<<endl;
+}
+int lomutoPartition(int arr[],int l,int h)
+{
+    int i=l-1;
+    int pivot=arr[h];
+    for(int j=l;j<=h-1;j++)
+    {
+        if(arr[j]<pivot)
+        {
+            i++; 
+            int temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+        }
+    }
+    int temp=arr[i+1];
+        arr[i+1]=arr[h];
+        arr[h]=temp;
+        return (i+1);
+}
+void quickSort(int arr[],int l,int h)
+{
+    if(l<h)
+    {                                            // THIS ALGORITHM FASETR THAN ANY ALGORITHM 
+        int p=lomutoPartition(arr,l,h);          // STABLE ALGORITHM
+        quickSort(arr,h,p-1);
+        quickSort(arr,p+1,h);
+    }
 }
 int main()
 {
@@ -21,6 +49,11 @@ int main()
         cin>>arr[i];
     }
     cout<<"Displaying your entered array elements \n";
+    display(arr,n);
+    cout<<"Index getting : "<<lomutoPartition(arr,0,n-1)<<endl;
+    display(arr,n);
+    quickSort(arr,0,n-1);
+    cout<<"Displaying your element using quick sort\n";
     display(arr,n);
 return 0;
 }
