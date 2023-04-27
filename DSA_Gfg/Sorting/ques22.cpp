1c4566bcaa81fdf12bc4a5330664bab3b328210f
sainiankushgithub <www.ankushsaini55@gmail.com>
2023-04-27 20:34:28 +0530

    programm

--- DSA_Gfg/Sorting/ques22.cpp
+++ DSA_Gfg/Sorting/ques22.cpp
@@ -1,6 +1,45 @@
 // QUICKSORT USING HOARE PARTITION
 #include<bits/stdc++.h>
 using namespace std;
+void display(int arr[],int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+int hoarePartition(int arr[],int l,int h)
+{
+    int i=l-1,j=h+1;
+    int pivot=arr[h];
+    while(true)
+    {
+        do{
+            i++;
+        }while(arr[i]<pivot);
+        do
+        {
+            j--;
+        } while (arr[j]>pivot);
+        if(i>=j)
+        {
+            return j;
+        }
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+}
+void QuickSort(int arr[],int l,int h)
+{
+    if(l<h)
+    {
+        int p=hoarePartition(arr,l,h);
+        QuickSort(arr,l,p);
+        QuickSort(arr,p+1,h);
+    }
+}
 int main()
 {
     int n;
@@ -14,10 +53,8 @@ int main()
     }
     cout<<"Displaying your entered array elements \n";
     display(arr,n);
-    cout<<"Index getting : "<<hoarePartition(arr,0,n-1)<<endl;
-    display(arr,n);
-    quickSort(arr,0,n-1);
-    cout<<"Displaying your element using quick sort\n";
+    QuickSort(arr,0,n-1);
+    cout<<"Displaying your after quick sort\n";
     display(arr,n);
 return 0;
 }
