7c6b70396273650814ca230b507ce69c7a08cd7c
sainiankushgithub <www.ankushsaini55@gmail.com>
2023-04-30 22:54:57 +0530

    programm

---
+++ DSA_Gfg/Sorting/ques30.cpp
@@ -0,0 +1,48 @@
+// SEGREGATE THE POSITIVE AND NEGATIVE 
+#include<bits/stdc++.h>
+using namespace std;
+void display(int arr[],int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+void segPosNeg(int arr[],int n)
+{
+    int i=-1;
+    int j=n;
+    while(true)
+    {
+        do{
+            i++;
+        }while(arr[i]<0);
+        do{
+            j--;
+        }while(arr[j]>=0);
+        if(i>=j)
+        {
+            return;
+        }
+        swap(arr[i],arr[j]);
+    }
+}
+int main()
+{
+    int n;
+    cout<<"Enter the size of an array\n";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter the numbers of an elements of an array\n";
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<"Displaying your array\n";
+    display(arr,n);
+    segPosNeg(arr,n);
+    cout<<"After Segregating the positive and negative elements our array become\n";
+    display(arr,n);
+return 0;
+}
