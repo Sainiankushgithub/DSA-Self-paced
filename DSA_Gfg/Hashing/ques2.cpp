b1be9561e9b6a38167ce3b3c27973494107a39e0
sainiankushgithub <www.ankushsaini55@gmail.com>
2023-05-17 23:26:42 +0530

    programm

--- DSA_Gfg/Hashing/ques2.cpp
+++ DSA_Gfg/Hashing/ques2.cpp
@@ -2,7 +2,24 @@
 // CHAINING IMPLEMENTATION
 #include<bits/stdc++.h>
 using namespace std;
+void display(int arr[],int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
 int main()
 {
+    int n;
+    cout<<"Enter the size of an array\n";
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    display(arr,n);
 return 0;
 }
