ed6b8a5531c45c7dbb7570389e09e1e939b3149f
sainiankushgithub <www.ankushsaini55@gmail.com>
2023-06-24 20:38:07 +0530

    programm

--- PracticeDSA/Mathematics1.cpp
+++ PracticeDSA/Mathematics1.cpp
@@ -22,11 +22,28 @@ bool checkprime(int n)
     }
     return true;
 }
+void displayprime(int num)
+{
+    int i=1;
+    while(i<num)
+    {
+        if(checkprime(i))
+        {
+            cout<<i<<" ";
+        }
+        i++;
+    }
+}
 int main()
 {
     int n;
     cout<<"Enter the number to check whether entered number is prime or not\n";
     cin>>n;
-    cout<<"Entered number is prime or not -> "<<checkprime(n);
+    cout<<"Entered number is prime or not -> "<<checkprime(n)<<endl;
+    int num;
+    cout<<"Enter the number to display the list of prime numbers upto your entered number\n";
+    cin>>num;
+    cout<<"Displaying the list of prime numbers\n";
+    displayprime(num);
 return 0;
 }
