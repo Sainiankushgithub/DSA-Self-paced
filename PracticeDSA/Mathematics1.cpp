55e97f701c8a38287989a73171caef91c881ac3c
sainiankushgithub <www.ankushsaini55@gmail.com>
2023-06-24 20:26:41 +0530

    programm

---
+++ PracticeDSA/Mathematics1.cpp
@@ -0,0 +1,32 @@
+// FINDING THE PRIME NUMBER USING NAVIE SOLUTION
+#include<bits/stdc++.h>
+using namespace std;
+bool checkprime(int n)
+{
+    if(n==1)
+    {
+        return true;
+    }
+    if(n==2)
+    {
+        return true;
+    }
+    int i=2;
+    while(i<n)
+    {
+        if(n%i==0)
+        {
+            return false;
+        }
+        i++;
+    }
+    return true;
+}
+int main()
+{
+    int n;
+    cout<<"Enter the number to check whether entered number is prime or not\n";
+    cin>>n;
+    cout<<"Entered number is prime or not -> "<<checkprime(n);
+return 0;
+}
