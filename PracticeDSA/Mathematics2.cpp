adea36f3b63e8a33b3a5d9d4cfd6470d6a62b8f1
sainiankushgithub <www.ankushsaini55@gmail.com>
2023-06-25 23:03:48 +0530

    programm

--- PracticeDSA/Mathematics2.cpp
+++ PracticeDSA/Mathematics2.cpp
@@ -1,6 +1,7 @@
 // ANOTEHR METHOD CHECKING THE NUMBER IS PRIME OR NOT
 #include<bits/stdc++.h>
 using namespace std;
+using namespace std::chrono;
 bool isprime(int n)
 {
     if(n==1)
@@ -24,11 +25,7 @@ bool isPrime(int num)
     {
         return false;
     }
-    if(num==2)
-    {
-        return true;
-    }
-    if(num==3)
+    if(num==2||num==3)
     {
         return true;
     }
@@ -36,17 +33,32 @@ bool isPrime(int num)
     {
         return false;
     }
-    
+    for(int i=5;i*i<=num;i=i+6)
+    {
+        if(num%i==0||num%(i+2)==0)
+        {
+            return false;
+        }
+    }
+    return true;
 }
 int main()
 {
+    // auto start=high_resolution_clock::now();
     int n;
     cout<<"Enter the number to check whether entered number is prime or not\n";
     cin>>n;
     cout<<"Entered number is prime or not -> "<<isprime(n)<<endl;
+    // auto stop=high_resolution_clock::now();
+    // auto duration=duration_cast<microseconds>(stop-start);
+    // cout<<"Time taken by code 1 is "<<duration.count()<<endl;
+    auto start=high_resolution_clock::now();
     int num;
     cout<<"Enter the number to check whether entered number is prime or not\n";
     cin>>num;
-    isPrime(num);
+    cout<<"Entered number id prime or not -> "<<isPrime(num)<<endl;
+    auto stop=high_resolution_clock::now();
+    auto duration=duration_cast<microseconds>(stop-start);
+    cout<<"Time taken by code 2 is : "<<duration.count()<<endl;
 return 0;
 }
