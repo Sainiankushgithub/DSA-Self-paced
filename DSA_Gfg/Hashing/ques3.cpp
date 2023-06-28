b13e87348fa83421de53a7614c38b89421f86979
sainiankushgithub <www.ankushsaini55@gmail.com>
2023-06-28 11:10:04 +0530

    programm

--- DSA_Gfg/Hashing/ques3.cpp
+++ DSA_Gfg/Hashing/ques3.cpp
@@ -1,25 +1,59 @@
-//CHAIN IMPLEMENTATION 
-/* 
-   1.
-   2.
-   3.
-*/
+// SIMPLE HASHING CODE 
 #include<bits/stdc++.h>
 using namespace std;
-class HashTable
-{
+class Hashtable{
+
    private:
-   list<int>table;
-   int size;
-   public:
-   HashTable(int size)
+
+   static const int Tablesize=10;                               // IT'S SIZE REMAIN SAME THROUGHOUT TEH EXECUTION OF THE PROGRAMM
+   list<int>hashtable[Tablesize];
+   int hashindex(int key)
    {
-      this->size=size;
-      table.resize(size);
+      return key%Tablesize;
    }
+
+   public :
+
+   void insert(int key);
+   void remove(int key);
+   void search(int key);
+   void display()
+   {
+      int index=0;
+      for(const auto &bucket:hashtable)
+      {
+         cout<<"Index "<<index++<<" : ";
+         for(auto it=bucket.begin();it!=bucket.end();it++)
+         {
+            cout<<*it<<" ";
+         }
+         cout<<endl;
+      }
+   }                                                                      
 };
+void Hashtable :: insert(int key)
+{
+   int index=hashindex(key);
+   hashtable[index].push_back(key);
+}
+void  Hashtable :: remove(int key)
+{
+   int index=hashindex(key);
+   hashtable[index].remove(key);
+}
 int main()
 {
-   HashTable h(10);
+   Hashtable h;
+   h.insert(5);
+   h.insert(15);
+   h.insert(25);
+   h.insert(35);
+   h.insert(45);
+   cout<<"Displaying your elements\n";
+   h.display();
+   cout<<"Removing the  element \n";
+   h.remove(15);
+   cout<<"After removing the specified element\n";
+   h.display();
 return 0;
 }
