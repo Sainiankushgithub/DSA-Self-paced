972f969310fe674f2f2e43c0898e5e700775f1aa
sainiankushgithub <www.ankushsaini55@gmail.com>
2023-07-06 22:45:43 +0530

    programm

---
+++ DSA_Gfg/Hashing/ques16.cpp
@@ -0,0 +1,43 @@
+// SUBARRAY WITH ZERO SUM 
+#include<bits/stdc++.h>
+using namespace std;
+void display(int arr[],int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+bool is0subarray(int arr[],int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        int sum=0;
+        for(int j=i;j<n;j++)
+        {
+            sum+=arr[j];
+            if(sum==0)
+            {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+int main()
+{
+    int n;
+    cout<<"Enter the size of an array\n";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter the number of an elements of an array\n";
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<"Displaying your array elements\n";
+    display(arr,n);
+    cout<<"Subarray wiht zero sum present in the array or not : "<<is0subarray(arr,n);
+return 0;
+}
