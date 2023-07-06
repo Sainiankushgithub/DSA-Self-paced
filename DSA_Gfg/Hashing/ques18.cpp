a7ce97a6ced67a12030c28ca2e77e1908dacd50c
sainiankushgithub <www.ankushsaini55@gmail.com>
2023-07-06 23:28:36 +0530

    programm

--- DSA_Gfg/Hashing/ques18.cpp
+++ DSA_Gfg/Hashing/ques18.cpp
@@ -1,4 +1,4 @@
-// SUB ARRAY WITH GIVEN SUM NAIVE SOLUTION
+// SUB ARRAY WITH GIVEN SUM EFFCIENT SOLUTIOLN
 #include<bits/stdc++.h>
 using namespace std;
 void display(int arr[],int n)
@@ -11,17 +11,20 @@ void display(int arr[],int n)
 }
 bool SubgivenSum(int arr[],int n,int sum)
 {
+    unordered_set<int>h;
+    int pre_sum=0;
     for(int i=0;i<n;i++)
     {
-        int curr_sum=0;
-        for(int j=i;j<n;j++)
+        pre_sum+=arr[i];
+        if(pre_sum==sum)
         {
-            curr_sum+=arr[j];
-            if(curr_sum==sum)
-            {
-                return true;
-            }
+            return true;
         }
+        if(h.find(pre_sum-sum)!=h.end())
+        {
+            return true;
+        }
+        h.insert(pre_sum);
     }
     return false;
 }
