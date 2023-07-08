b9c0f3cb6d7e350afbb1ca7b10c683b45224b6b4
sainiankushgithub <www.ankushsaini55@gmail.com>
2023-07-08 23:33:41 +0530

    programm

--- DSA_Gfg/Hashing/ques22.cpp
+++ DSA_Gfg/Hashing/ques22.cpp
@@ -1,7 +1,7 @@
 //  LONGEST SUBARRAY WITH EQUAL 0s and 1s NAIVE SOLUTION
 #include<bits/stdc++.h>
 using namespace std;
-void display(int arr[],int n)
+void display(bool arr[],int n)
 {
     for(int i=0;i<n;i++)
     {
@@ -9,13 +9,36 @@ void display(int arr[],int n)
     }
     cout<<endl;
 }
-
+int LonSubArray(bool arr[],int n)
+{
+    int res=0;
+    for(int i=0;i<n;i++)
+    {
+        int c0=0,c1=0;
+        for(int j=i;j<n;j++)
+        {
+            if(arr[j]==0)
+            {
+                c0++;
+            }
+            else
+            {
+                c1++;
+            }
+            if(c0==c1)
+            {
+                res=max(res,c0+c1);
+            }
+        }
+    }
+    return res;
+}
 int main()
 {
     int n;
     cout<<"Enter the size of an array\n";
     cin>>n;
-    int arr[n];
+    bool arr[n];
     cout<<"Enter the number of an elements of an array\n";
     for(int i=0;i<n;i++)
     {
@@ -23,5 +46,6 @@ int main()
     }
     cout<<"Displaying your array elements\n";
     display(arr,n);
+    cout<<"Longest sub array with 0s and 1s is : "<<LonSubArray(arr,n)<<endl;
 return 0;
 }
