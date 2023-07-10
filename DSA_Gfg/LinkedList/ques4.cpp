520f1e4a4d281320819f009d4a86bffb951e1fab
sainiankushgithub <www.ankushsaini55@gmail.com>
2023-07-10 23:16:20 +0530

    programm

--- DSA_Gfg/LinkedList/ques4.cpp
+++ DSA_Gfg/LinkedList/ques4.cpp
@@ -25,9 +25,32 @@ Node* InsertFunc(Node* head,int x)
         {
             temp=temp->next;
         }
+        temp->next=newnode;
+    }
+    return head;
+}
+void display(Node*head)
+{
+    Node*temp=head;
+    while(temp!=nullptr)
+    {
+        cout<<(temp->data)<<endl;
+        temp=temp->next;
     }
 }
 int main()
 {
+    Node*head=nullptr;
+    int n;
+    cout<<"Enter the number of node\n";
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        int x;
+        cin>>x;
+        head=InsertFunc(head,x);
+    }
+    cout<<"Displaying your data : \n";
+    display(head);
 return 0;
 }
