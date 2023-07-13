3728ddd300706be5d0f8c89cc9d48cda188945b9
sainiankushgithub <www.ankushsaini55@gmail.com>
2023-07-13 23:10:43 +0530

    programm

---
+++ DSA_Gfg/Stack/ques4.cpp
@@ -0,0 +1,27 @@
+// CHECK FOR BALANCCED PARETHESIS 
+#include<bits/stdc++.h>
+using namespace std;
+bool isBalanced(string str)
+{
+    stack<char>s;
+
+}
+bool matching(char a,char b)
+{
+    
+}
+int main()
+{
+    string str;
+    cout<<"Enter the parethesis :\n";
+    cin>>str;
+    if(isBalanced(str)==true)
+    {
+    cout<<"YES  ! Parethesis matching :\n";
+    }
+    else
+    {
+        cout<<"NO  ! Parethesis not matching :\n";
+    }
+return 0;
+}
