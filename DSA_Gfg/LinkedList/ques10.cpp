27a529d3ce9b6c40d9f86b459c7b41cd3f300384
sainiankushgithub <www.ankushsaini55@gmail.com>
2023-07-17 22:59:35 +0530

    programm

--- DSA_Gfg/LinkedList/ques10.cpp
+++ DSA_Gfg/LinkedList/ques10.cpp
@@ -1,11 +1,39 @@
 // SEARCH IN A LINKED LIST 
 #include<bits/stdc++.h>
 using namespace std;
-struct Node
-{
-
+struct Node {
+    int data;
+    Node* next;
 };
-int main()
-{
-return 0;
+bool searchLinkedList(Node* head,int value) {
+    Node* current=head;
+    while (current!=nullptr) {
+        if (current->data==value) {
+            return true;
+        }
+        current = current->next;
+    }
+    return false;
+}
+int main() {
+    Node* node1 =new Node();
+    Node* node2 =new Node();
+    Node* node3 =new Node();
+    node1->data=10;
+    node2->data=20;
+    node3->data=30;
+    node1->next=node2;
+    node2->next=node3;
+    node3->next=nullptr;
+    int value = 20;
+    if (searchLinkedList(node1,value)) {
+        cout<<"Value " << value<<" found in the linked list."<<endl;
+    } else {
+        cout<<"Value " << value<<" not found in the linked list."<<endl;
+    }
+    delete node1;
+    delete node2;
+    delete node3;
+
+    return 0;
 }
