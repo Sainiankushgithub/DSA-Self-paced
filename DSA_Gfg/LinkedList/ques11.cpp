ebd18724ac3fa3f0c7dd21b5feea5768a2b923e5
sainiankushgithub <www.ankushsaini55@gmail.com>
2023-07-18 11:45:49 +0530

    programm

--- DSA_Gfg/LinkedList/ques11.cpp
+++ DSA_Gfg/LinkedList/ques11.cpp
@@ -1,11 +1,52 @@
-// DOUBLY LINKED LIST 
+// RECURSIVE SEARCH IN LINKED LIST 
 #include<bits/stdc++.h>
 using namespace std;
-struct Node {
+struct Node 
+{
     int data;
     Node* next;
+    Node(int x)
+    {
+        data=x;
+        next=NULL;
+    }
 };
+void display(Node*head)
+{
+    while(head!=nullptr)
+    {
+        cout<<head->data<<" ";
+        head=head->next;
+    }
+    cout<<endl;
+}
+bool searchele(Node*head,int value)
+{
+    if(head==nullptr){
+        return false;
+    }
+    if(head->data==value)
+    {
+        return true;
+    }
+    return searchele(head->next,value);
+}
 int main()
 {
+    Node*head=new Node(10);
+    head->next=new Node(20);
+    head->next->next=new Node(30);
+    head->next->next->next=new Node(40);
+    head->next->next->next->next=new Node(50);
+    cout<<"Displaying your list elements :\n";
+    display(head);
+    if(searchele(head,20))
+    {
+        cout<<"Element found : "<<endl;
+    }
+    else
+    {
+        cout<<"Element not found : "<<endl;
+    }
 return 0;
 }
