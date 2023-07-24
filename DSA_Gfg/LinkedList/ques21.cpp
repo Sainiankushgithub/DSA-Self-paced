7706c0bde5a275d812a7a88788a03cfb8d1b569d
sainiankushgithub <www.ankushsaini55@gmail.com>
2023-07-24 14:13:24 +0530

    programm

--- DSA_Gfg/LinkedList/ques21.cpp
+++ DSA_Gfg/LinkedList/ques21.cpp
@@ -1,42 +1,37 @@
-// REVERSE A DOUBLY LINKED LIST
+// IMPLEMENTATION OF A CIRCULAR LINKED LIST 
 #include<bits/stdc++.h>
 using namespace std;
 struct Node
 {
     int data;
     Node*next;
-    Node*prev;
     Node(int x)
     {
         data=x;
         next=NULL;
-        prev=NULL;
     }
 };
 void display(Node*head)
 {
-    Node*curr=head;
-    while(curr!=head)
+    if(head==NULL)
     {
-        cout<<curr->data<<" ";
-        curr=curr->next;
+        return ;
     }
-    cout<<endl;
-}
-Node*ReverseDoubly(Node*head)
-{
-    
+    cout<<head->data<<" ";
+    Node*p=head->next;
+    while(p!=head)
+    {
+        cout<<p->data<<" ";
+        p=p->next;
+    }
 }
 int main()
 {
-    /* DOUBLY LINKED LIST IMPLEMENTATION */
     Node*head=new Node(10);
     head->next=new Node(20);
-    head->next->prev=head;
     head->next->next=new Node(30);
-    head->next->next->prev=head->next;
+    head->next->next->next=head;
     cout<<"Displaying your doubly linked list :\n";
     display(head);
-    head=ReverseDoubly(head);
 return 0;
 }
