04b14b7fe110e409e16b62ab14e5f2d8281bac6d
sainiankushgithub <www.ankushsaini55@gmail.com>
2023-07-27 22:56:23 +0530

    programm

---
+++ DSA_Gfg/LinkedList/ques40.cpp
@@ -0,0 +1,39 @@
+// REVERSE A LINKED LIST USING RECURSION 
+#include<bits/stdc++.h>
+using namespace std;
+struct Node
+{
+    int data;
+    Node*next;
+    Node(int x)
+    {
+        data=x;
+        next=NULL;
+    }
+};
+void display(Node*head)
+{
+    while(head!=nullptr)
+    {
+        cout<<head->data<<" ";
+        head=head->next;
+    }
+    cout<<endl;
+}
+Node*ReverseLinkedList(Node*head)
+{
+    
+}
+int main()
+{
+    Node*head=new Node(10);
+    head->next=new Node(20);
+    head->next->next=new Node(30);
+    head->next->next->next= new Node(40);
+    cout<<"Displaying your linked list :\n";
+    display(head);
+    head=ReverseLinkedList(head);
+    cout<<"Displaying your linked list in Reverse order :\n";
+    display(head);
+return 0;
+}
