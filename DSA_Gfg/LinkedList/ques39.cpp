89362a87b9924864b8fccf9c7bf59c4559dfa3f1
sainiankushgithub <www.ankushsaini55@gmail.com>
2023-07-28 11:54:26 +0530

    programm

--- DSA_Gfg/LinkedList/ques39.cpp
+++ DSA_Gfg/LinkedList/ques39.cpp
@@ -23,7 +23,15 @@ void display(Node*head)
 Node*ReverseLinkedList(Node*head)
 {
     Node*curr=head;
-    
+    Node*prev=NULL;
+    while(curr!=NULL)
+    {
+        Node*next=curr->next;
+        curr->next=prev;
+        prev=curr;
+        curr=next;
+    }
+    return prev;
 }
 int main()
 {
