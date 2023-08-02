99c3520b4707052a1cd47df5aa5c116d63307a88
sainiankushgithub <www.ankushsaini55@gmail.com>
2023-08-02 22:04:49 +0530

    programm

---
+++ DSA_Gfg/LinkedList/ques52.cpp
@@ -0,0 +1,67 @@
+// CHECK PALINDROME EFFICIENT SOLUTION 
+#include<bits/stdc++.h>
+using namespace std;
+struct Node
+{
+    char data;
+    Node*next;
+    Node(char x)
+    {
+        data=x;
+        next=nullptr;
+    }
+};
+void display(Node*head)
+{
+    while(head!=NULL)
+    {
+        cout<<head->data<<" ";
+        head=head->next;
+    }
+    cout<<endl;
+}
+Node* InsertFun(Node*head,char x)
+{
+    Node*temp=new Node(x);
+    if(head==NULL)
+    {
+        head=temp;
+        return head;
+    }
+    Node*curr=head;
+    while(curr->next!=nullptr)
+    {
+        curr=curr->next;
+    }
+    curr->next=temp;
+    return head;
+}
+string checkPalindrome(Node*head)
+{
+    
+}
+int main()
+{
+    Node*head=NULL;
+    int n;
+    cout<<"Enter the number of nodes :\n";
+    cin>>n;
+    cout<<"Enter the linked list data :\n";
+    for(int i=0;i<n;i++)
+    {
+        char x;
+        cin>>x;
+        head=InsertFun(head,x);
+    }
+    cout<<"Displaying the linked list data :\n";
+    display(head);
+    if(checkPalindrome(head)=="Yes")
+    {
+        cout<<"Entered characters are palindrome :\n";
+    }
+    else
+    {
+        cout<<"Entered characters are not  palindrome :\n";
+    }
+return 0;
+}
