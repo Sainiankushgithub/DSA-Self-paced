871121bca75f15fe3f72d0c95e538418d27d0617
sainiankushgithub <www.ankushsaini55@gmail.com>
2023-08-03 21:27:14 +0530

    programm

---
+++ DSA_Gfg/LinkedList/ques53.cpp
@@ -0,0 +1,74 @@
+// MERGE TWO SORTED LINKED list 
+#include<bits/stdc++.h>
+using namespace std;
+struct Node
+{
+    int data;
+    Node*next;
+    Node(int x)
+    {
+        data=x;
+        next=nullptr;
+    }
+};
+void display(Node*head)
+{
+    while(head!=nullptr)
+    {
+        cout<<head->data<<" ";
+        head=head->next;
+    }
+    cout<<endl;
+}
+Node*InsertFun(Node*head,int x)
+{
+    Node*temp=new Node(x);
+    if(head==NULL)
+    {
+        head=temp;
+        return head;
+    }
+    Node*curr=head;
+    while(curr->next!=nullptr)
+    {
+        curr=curr->next;
+    }
+    curr->next=temp;
+    return head;
+}
+Node*MergeTwolist(Node*head1,Node*head2)
+{
+   
+}
+int main()
+{
+    Node*head1=nullptr;
+    Node*head2=nullptr;
+    int n1,n2;
+    cout<<"Enter the number of nodes you want to enter in linked list 1 :\n";
+    cin>>n1;
+    cout<<"Enter the number of nodes you want to enter in linked list 2 :\n";
+    cin>>n2;
+    cout<<"Enter the numbers of elements of linked list 1 :\n";
+    for(int i=0;i<n1;i++)
+    {
+        int x;
+        cin>>x;
+        head1=InsertFun(head1,x);
+    }
+    cout<<"Enter the numbers of elements of linked list 2 :\n";
+    for(int i=0;i<n1;i++)
+    {
+        int x;
+        cin>>x;
+        head2=InsertFun(head2,x);
+    }
+    cout<<"Displaying your linked list 1 :\n";
+    display(head1);
+    cout<<"Displaying your linked list 2 :\n";
+    display(head2);
+    Node*head=MergeTwolist(head1,head2);
+    cout<<"Displaying your two sorted list :\n";
+    display(head);
+return 0;
+}
