ac09633a5e4fcd2fade5df0974a8746de1f4032c
sainiankushgithub <www.ankushsaini55@gmail.com>
2023-08-05 13:30:19 +0530

    programm

---
+++ DSA_Gfg/Stack/ques9.cpp
@@ -0,0 +1,50 @@
+// EFFICINET SOLUTION  MY SOLUTION 
+#include<bits/stdc++.h>
+using namespace std;
+void display(int arr[],int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+void NearestGreater(int arr[],int n)
+{
+    stack<int>s;
+    s.push(arr[0]);
+    cout<<-1<<" ";
+    for(int i=1;i<n;i++)
+    {
+        while(s.empty()==false&&s.top()<arr[i])
+        {
+            s.pop();
+        }
+        if(s.empty()==true)
+        {
+            s.push(arr[i]);
+            cout<<-1<<" ";
+        }
+        else
+        {
+            cout<<s.top()<<" ";
+            s.push(arr[i]);
+        }
+    }
+}
+int main()
+{
+    int n;
+    cout<<"Enter the size of an array :\n";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter the numbers of an elements of an array :\n";
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<"Displaying your array elements :\n";
+    display(arr,n);
+    NearestGreater(arr,n);
+return 0;
+}
