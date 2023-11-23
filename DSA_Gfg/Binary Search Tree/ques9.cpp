9a0cdc9e78c122f7fa82420fa59a853d8247ce59
sainiankushgithub <www.ankushsaini55@gmail.com>
2023-11-23 23:08:42 +0530

    programm

---
+++ DSA_Gfg/Binary Search Tree/ques9.cpp
@@ -0,0 +1,46 @@
+// SET IN C++ 
+#include<bits/stdc++.h>
+using namespace std;
+struct Node
+{
+    int data;
+    Node*left,*right;
+    Node(int x)
+    {
+        data=x;
+        left=right=NULL;
+    }
+};
+Node*InsertNode(Node*root,int x)
+{
+    if(root==NULL)
+    {
+        return new Node(x);
+    }
+    else if(root->data>x)
+    {
+        root->left=InsertNode(root->left,x);
+    }
+    else
+    {
+        root->right=InsertNode(root->right,x);
+    }
+    return root;
+}
+int main()
+{
+    Node*root=NULL;
+    int height;
+    cout<<"Enter the height of the binary tree \n";
+    cin>>height;
+    cout<<"Enter the Binary search data according to your height \n";
+    for(int i=0;i<height;i++)
+    {
+        int x;
+        cin>>x;
+        root=InsertNode(root,x);
+    }
+    // written there left !
+    
+return 0;
+}
