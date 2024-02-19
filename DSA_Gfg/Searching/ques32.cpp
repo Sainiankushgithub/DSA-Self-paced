744093be9306839da8191f8c9c156c770d675007
SainiAnkushgithub <www.ankushsaini55@gmail.com>
2024-02-19 23:12:04 +0530

    SearchInserIndex

--- DSA_Gfg/Searching/ques32.cpp
+++ DSA_Gfg/Searching/ques32.cpp
@@ -1,9 +1,15 @@
 // SEARCH INSERT INDEX 
 #include<bits/stdc++.h>
 using namespace std;
+int SearchIndex(int arr[],int size)
+{
+    // CODE
+}
 int main()
 {
     int arr[]={2,4,6,7,9,11,21,34,43,45};
     int size=sizeof(arr)/sizeof(int);
+    
+    int index=SearchIndex(arr,size);
 return 0;
 }
