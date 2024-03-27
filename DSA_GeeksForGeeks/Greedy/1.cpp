99e4bfbbc4629a766a54b7ad1ee5a9050afdc8a0
SainiAnkushgithub <www.ankushsaini55@gmail.com>
2024-03-27 23:36:22 +0530

    Activity selection problem

--- DSA_GeeksForGeeks/Greedy/1.cpp
+++ DSA_GeeksForGeeks/Greedy/1.cpp
@@ -1,17 +1,43 @@
 // ACTIVITY SELECTION PROBLEM
 #include<bits/stdc++.h>
 using namespace std;
-int main()
+void display(vector<vector<int>>&pair)
 {
-  vector<vector<int>>v={{2,3},{1,4},{5,8},{6,10}};
-  for(int i=0;i<v.size();i++)
+  for(int i=0;i<pair.size();i++)
   {
-    for(int j=0;j<v[i].size();j++)
+    for(int j=0;j<pair[i].size();j++)
     {
-      cout<<v[i][j]<<" ";
+      cout<<pair[i][j]<<" ";
     }
     cout<<endl;
   }
-  
+}
+int maxCount(vector<vector<int>>&pair)
+{
+  int res=1,count=1;
+  for(int i=0;i<pair.size()-1;i++)
+  {
+    int initial=pair[i][1];
+    count=1;
+    for(int k=i+1;k<pair.size();k++)
+    {
+      int final=pair[k][0];
+      if(initial<=final)
+      {
+        count++;
+        res=max(res,count);
+        initial=pair[k][1];
+      }
+    }
+  }
+  return res;
+}
+int main()
+{
+  // vector<vector<int>>pair={{2,3},{1,4},{5,8},{6,10}};
+  vector<vector<int>>pair={{1,3},{2,4},{3,8},{10,11}};
+  cout<<"Displaying your vector "<<endl;
+  display(pair);
+  cout<<"Maximum count : "<<maxCount(pair)<<endl;
 return 0;
 }
