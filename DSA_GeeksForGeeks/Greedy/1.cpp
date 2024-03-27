7b54e1682b18774203d9d547fc5f4ae010f5ded1
SainiAnkushgithub <www.ankushsaini55@gmail.com>
2024-03-27 22:56:05 +0530

    Activity Selection problem

--- DSA_GeeksForGeeks/Greedy/1.cpp
+++ DSA_GeeksForGeeks/Greedy/1.cpp
@@ -1,6 +1,17 @@
+// ACTIVITY SELECTION PROBLEM
 #include<bits/stdc++.h>
 using namespace std;
 int main()
 {
+  vector<vector<int>>v={{2,3},{1,4},{5,8},{6,10}};
+  for(int i=0;i<v.size();i++)
+  {
+    for(int j=0;j<v[i].size();j++)
+    {
+      cout<<v[i][j]<<" ";
+    }
+    cout<<endl;
+  }
+  
 return 0;
 }
