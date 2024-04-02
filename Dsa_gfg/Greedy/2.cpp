ecab0af8ada6261dbbc9641aad09f22fc1f6e15b
SainiAnkushgithub <www.ankushsaini55@gmail.com>
2024-04-02 23:04:09 +0530

    minimum number of coins

--- Dsa_gfg/Greedy/2.cpp
+++ Dsa_gfg/Greedy/2.cpp
@@ -1,6 +1,18 @@
+// MINIMUM COIN PROBLEM 
 #include<bits/stdc++.h>
 using namespace std;
 int main()
 {
+  int n;
+  cout<<"Enter the total number of a coin \n";
+  cin>>n;
+  vector<int>arr;
+  cout<<"Enter the coins you have !!!\n";
+  for(int i=0;i<n;i++)
+  {
+    int x;
+    cin>>x;
+    arr.push_back(x);
+  }
 return 0;
 }
