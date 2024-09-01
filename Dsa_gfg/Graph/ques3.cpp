1da9ffc0bbaa625db3a36d6abf32c1b6cd0bdd25
SainiAnkushgithub <www.ankushsaini55@gmail.com>
2024-09-01 14:07:45 +0530

    adjacency matrix & adjacency List

--- Dsa_gfg/Graph/ques3.cpp
+++ Dsa_gfg/Graph/ques3.cpp
@@ -1,18 +1,27 @@
-// DETECT CYCLE IN UNDIRECTED GRAPH
+// Adjacency matrix 
 #include<bits/stdc++.h>
 using namespace std;
 int main()
 {
-    int vertices;
-    cout<<"Enter the vertices \n";
-    cin>>vertices;
-    int graph[vertices][vertices];
-    for(int i=0;i<vertices;i++)
+    int node,edges;
+    cout<<"Enter the number of nodes ans edges\n";
+    cin>>node>>edges;
+    vector<vector<int>>adj(node+1,vector<int>(node+1,0));
+    cout<<"Enter the edge which is connected\n";
+    for(int i=0;i<edges;i++)
     {
-        for(int j=0;j<vertices;j++)
+        int e1,e2;
+        cin>>e1>>e2;
+        adj[e1][e2]=1;
+        adj[e2][e1]=1;
+    }
+    for(int i=0;i<node+1;i++)
+    {
+        for(int j=0;j<node+1;j++)
         {
-            
+            cout<<adj[i][j]<<" ";
         }
+        cout<<endl;
     }
 return 0;
 }
--- Dsa_gfg/Graph/ques4.cpp
+++ Dsa_gfg/Graph/ques4.cpp
@@ -1,9 +1,31 @@
-// DETECT CYCLE IN DIRECTED GRAPH PART(2)
-
+// Adjacency list 
 #include<bits/stdc++.h>
 using namespace std;
 int main()
 {
-  
+  int nodes,edges;
+  cout<<"Enter the number of nodes \n";
+  cin>>nodes;
+  cout<<"Enter the total number of edges\n";
+  cin>>edges;
+  vector<int>adjList[nodes+1];
+  cout<<"Enter the nodes of the edge\n";
+  for(int i=0;i<edges;i++)
+  {
+    int n1,n2;
+    cin>>n1>>n2;
+    adjList[n1].push_back(n2);
+    adjList[n2].push_back(n1);
+  }
+  cout<<"Node connected to given edges are \n";
+  for(int i=1;i<=nodes;i++)
+  {
+    cout<<"Node : "<<i<<"-> ";
+    for(auto it:adjList[i])
+    {
+      cout<<it<<" ";
+    }
+    cout<<endl;
+  }
 return 0;
 }
