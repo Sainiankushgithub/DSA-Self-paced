3b03e3679e309cb8e8ac21bc283b7893638942b3
SainiAnkushgithub <www.ankushsaini55@gmail.com>
2025-08-10 23:20:27 +0530

    Function Comparator

--- Dsa_gfg/Comparators/FunctionPointer1.cpp
+++ Dsa_gfg/Comparators/FunctionPointer1.cpp
@@ -10,6 +10,27 @@ void display(vector<int>&v){
 bool myComparator(int a ,int b){
   return a>b?true:false;
 }
+bool myComparatorsPair(pair<int,int>p1,pair<int,int>p2){
+  /*
+      Below one is working only for the first greater element in the pair 
+  */
+  //  return p1.first>p2.first;
+
+  /*
+      This is for both the greater element if we have 2 pair with first same element but which one is greater that will be taken first
+  */
+
+  if(p1.first==p2.first){
+    return p1.second>p2.second;
+  }
+  return p1.first>p2.first;
+}
+
+void displaypair(vector<pair<int,int>>&vp){
+  for(auto it:vp){
+    cout<<"{"<<it.first<<","<<it.second<<"}, ";
+  }
+}
 int main()
 {
   vector<int> vec = {0, 7, 4, 1, 0, 3, 2, 9, 4, 10};
@@ -22,6 +43,14 @@ int main()
   display(vec);
 
   // Let Assume vector of pair 
-  
+  vector<pair<int,int>>vp={{2,0},{4,0},{4,1},{1,2},{1,9},{0,2},{2,3}};
+  // By Default sorting 
+  // sort(begin(vp),end(vp));
+  // displaypair(vp);
+
+  // suppose I wanna sort above element in descending order 
+
+  sort(begin(vp),end(vp),myComparatorsPair);
+  displaypair(vp);
   return 0;
 }
