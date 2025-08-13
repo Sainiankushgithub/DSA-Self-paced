afd26861e54375c4b9489e334fb38b3efed9ece0
SainiAnkushgithub <www.ankushsaini55@gmail.com>
2025-08-13 19:15:46 +0530

    update

--- Dsa_gfg/Vector/Iterator.cpp
+++ Dsa_gfg/Vector/Iterator.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 int main()
 {
-  vector<int> v = {3, 1, 9, 5, 2, 7, 4};
-    int index = 4; // Find max element till index 4 (inclusive)
+  vector<int> v = {3,11, 1, 9, 5, 2, 7, 4,10};
+    int index = v.size();
 
     // Using max_element to find the maximum element in the range [v.begin(), v.begin() + index + 1)
-    auto max_it = max_element(v.begin(), v.begin() + index + 1);
+    auto max_it = max_element(v.begin()+2, v.begin() + index);
 
     // Dereference the iterator to get the value
     int max_value = *max_it;
